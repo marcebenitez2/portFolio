2a98921d47ad606e81f621b1a278d4a886378490
Marce Benitez <106833298+marcebenitez2@users.noreply.github.com>
2023-09-14 15:47:39 -0300

    iniciando portfolio

---
+++ tailwind.config.cjs
@@ -0,0 +1,43 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+	screens: {
+	'sm': '640px',   
+	'md': '768px', 
+	'lg': '1024px',
+	'xl': '1280px', 
+  
+	'2xln': { max: "1535px" },
+	// => @media (max-width: 1535px) { ... }
+  
+	'xln': { max: "1279px" },
+	// => @media (max-width: 1279px) { ... }
+  
+	'lgn': { max: "1023px" },
+	// => @media (max-width: 1023px) { ... }
+  
+	'mdn': { max: "767px" },
+	// => @media (max-width: 767px) { ... }
+  
+	'smn': { max: "639px" },
+	// => @media (max-width: 639px) { ... }
+  
+	  },
+    extend: {
+      colors: {
+        "custom-black" : "#000000",
+        "custom-purple": "#26184b",
+        "custom-blue": "#3E3062",
+        "custom-pink": {
+          light: "#F881F3",
+          DEFAULT: "#BC81F8",
+        },
+        "custom-lavender": "#F0DAFB",
+        "custom-white": "#FFFFFF",
+      },
+    },
+  },
+  darkMode: "class",
+  plugins: [],
+};
