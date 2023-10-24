9afc31220f0775e5f658b3e69a7caa8fb5a4a976
marcebenitez2 <marcebenitez0607@gmail.com>
2023-10-24 15:14:26 -0300

    Cambios de proyectos

--- src/services/arrayProjects.js
+++ src/services/arrayProjects.js
@@ -3,18 +3,22 @@ export const proyectos = [
       id: 1,
       nombre: "VespFlix",
       descripcion:
-        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. At odit debitis numquam itaque, quasi minima tempora dignissimos quae quo magni possimus! Dolores voluptatem ullam sint cum minus, atque enim totam.",
+        "Elabore una pagina web de peliculas y series, donde obtendras informacion de ellas y podras guardarlas en tu lista de favoritos.",
+        style: 'bg-gradient-to-br from-purple-700 via-fuchsia-500 to-red-600 bg-clip-text text-transparent text-3xl font-semibold',
+        tecnologias:
     },
     {
       id: 2,
       nombre: "Scout Proyect",
       descripcion:
-        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. At odit debitis numquam itaque, quasi minima tempora dignissimos quae quo magni possimus! Dolores voluptatem ullam sint cum minus, atque enim totam.",
+        "Desarrolle una aplicación web para el Grupo Scout San Miguel de Arcángel. Usuarios externos podrán acceder a información sobre actividades, fotos y contactar al grupo. Los miembros gestionarán información interna en una sección dedicada.",
+        style: 'text-3xl font-semibold text-red-500'
     },
     {
       id: 3,
       nombre: "Calcos Blessed",
       descripcion:
-        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. At odit debitis numquam itaque, quasi minima tempora dignissimos quae quo magni possimus! Dolores voluptatem ullam sint cum minus, atque enim totam.",
+        "Codifique un E-Commerce para la venta de calcomanias. Los usuarios podrán ver los productos, agregarlos al carrito y realizar la compra. Los administradores podrán gestionar los productos, categorías y pedidos.",
+        style:'text-3xl font-semibold text-yellow-500'
     },
   ];
