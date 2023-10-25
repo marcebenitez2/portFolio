eadc8a0cfb20e999fb11bf4a1dd1e93e5f2272ef
marcebenitez2 <marcebenitez0607@gmail.com>
2023-10-24 21:27:32 -0300

    svg logo agregados

--- src/services/arrayProjects.js
+++ src/services/arrayProjects.js
@@ -5,20 +5,22 @@ export const proyectos = [
       descripcion:
         "Elabore una pagina web de peliculas y series, donde obtendras informacion de ellas y podras guardarlas en tu lista de favoritos.",
         style: 'bg-gradient-to-br from-purple-700 via-fuchsia-500 to-red-600 bg-clip-text text-transparent text-3xl font-semibold',
-        tecnologias:
+        tecnologias: ['react.svg','react-router.svg','axios.svg','tmdb.svg']
     },
     {
       id: 2,
       nombre: "Scout Proyect",
       descripcion:
         "Desarrolle una aplicación web para el Grupo Scout San Miguel de Arcángel. Usuarios externos podrán acceder a información sobre actividades, fotos y contactar al grupo. Los miembros gestionarán información interna en una sección dedicada.",
-        style: 'text-3xl font-semibold text-red-500'
+        style: 'text-3xl font-semibold text-red-500',
+        tecnologias: ['react.svg','new-php-logo.svg','mysql.svg','react-router.svg']
     },
     {
       id: 3,
       nombre: "Calcos Blessed",
       descripcion:
         "Codifique un E-Commerce para la venta de calcomanias. Los usuarios podrán ver los productos, agregarlos al carrito y realizar la compra. Los administradores podrán gestionar los productos, categorías y pedidos.",
-        style:'text-3xl font-semibold text-yellow-500'
+        style:'text-3xl font-semibold text-yellow-500',
+        tecnologias: ['react.svg','next.png','firebase.png','react-router.svg']
     },
   ];
